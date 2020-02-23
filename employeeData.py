bbe39d46ad93fe312b5e6eae94ff221d08690044
Sheikh Imran <imranjustcse@gmail.com>
2020-02-23 20:59:10 +0600

    Update employeeData.py

--- employeeData.py
+++ employeeData.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from pandas import read_excel
-from functools import reduce
 from pandas import ExcelWriter
 
 from fuzzywuzzy import fuzz
@@ -14,26 +13,26 @@ class Employee:
         regex1 = '\(.*?\)'
         regex2 = '\d'
         regex3 = '\w-'
-        dataframe['Name'].replace(regex=True, inplace=True, to_replace=[regex1, regex2, regex3], value=r'')
+        regex4 = '^md.'
+        dataframe['Name'].replace(regex=True, inplace=True, to_replace=[regex1, regex2, regex3, regex4], value=r'')
         return dataframe
     
     def make_name_lower_case(self, dataframe):
-        dataframe['Name'] = dataframe['Name'].str.lower()
+        dataframe['name'] = dataframe['Name']
+        dataframe['name'] = dataframe['name'].str.lower()
         return dataframe
 
     def remove_white_space(self, dataframe):
-        dataframe['Name'] = dataframe['Name'].str.replace(' ', '')
+        dataframe['name'] = dataframe['name'].str.replace(' ', '')
         return dataframe
     
+#    def match_dataframe_by_name(self, df1, df2, df3):
+#        employee_dataframe = pd.merge(pd.merge(df1,df2,on='Name'),df3,on='Name')
+#        return employee_dataframe
+#        dataframes = [df1, df2, df3]
+#        return reduce(lambda left,right: pd.merge(left,right,on='Name'), dataframes)
+    
     def fuzzy_merge_df1_and_df2(self, df_1, df_2, key1, key2, threshold=50, limit=2):
-        """
-        df_1 is the left table to join
-        df_2 is the right table to join
-        key1 is the key column of the left table
-        key2 is the key column of the right table
-        threshold is how close the matches should be to return a match, based on Levenshtein distance
-        limit is the amount of matches that will get returned, these are sorted high to low
-        """
         s = df_2[key2].tolist()
     
         m = df_1[key1].apply(lambda x: process.extract(x, s, limit=limit))    
@@ -46,14 +45,6 @@ class Employee:
         return df_1
     
     def fuzzy_merge_df1_df2_df3(self, df_1, df_2, key1, key2, threshold=50, limit=2):
-        """
-        df_1 is the left table to join
-        df_2 is the right table to join
-        key1 is the key column of the left table
-        key2 is the key column of the right table
-        threshold is how close the matches should be to return a match, based on Levenshtein distance
-        limit is the amount of matches that will get returned, these are sorted high to low
-        """
         s = df_2[key2].tolist()
     
         m = df_1[key1].apply(lambda x: process.extract(x, s, limit=limit))    
@@ -68,7 +59,12 @@ class Employee:
     def write_dataframe_to_exel(self, dataframe):
         writer = ExcelWriter('EmployeeExport.xlsx')
         dataframe.to_excel(writer,'Sheet')
-        writer.save()        
+        writer.save()   
+        
+    def remove_columns(self, dataframe, **kwargs):
+        columns = [kwargs['name'], kwargs['matches']]
+        dataframe.drop(columns, inplace=True, axis=1)
+        return dataframe
        
 
 if __name__ == '__main__':
@@ -77,8 +73,8 @@ if __name__ == '__main__':
 
     file_name1 = 'Raw Employee(odoo).xlsx'
     df1 = employee.read_xl_file(file_name1)
-    df1 = employee.preprocess_data_with_regex(df1)
     df1 = employee.make_name_lower_case(df1)
+    df1 = employee.preprocess_data_with_regex(df1)
     df1 = employee.remove_white_space(df1)
     
     file_name2 = 'Device Employee.xlsx'
@@ -88,23 +84,20 @@ if __name__ == '__main__':
     df2 = employee.remove_white_space(df2)
     
     # match dataframes by name
-    df1_and_df2 = employee.fuzzy_merge_df1_and_df2(df1, df2, 'Name', 'Name', threshold=50)
+    df1_and_df2 = employee.fuzzy_merge_df1_and_df2(df1, df2, 'name', 'name', threshold=75)
         
     file_name3 = 'all employee list.xls'
     df3 = employee.read_xl_file(file_name3)
     df3 = employee.preprocess_data_with_regex(df3)
     df3 = employee.make_name_lower_case(df3)
     df3 = employee.remove_white_space(df3)
+
+#    employee_dataframe = employee.match_dataframe_by_name(df1, df2, df3)
+#    final_dataframe = employee.get_specific_columns_from_dataframe(employee_dataframe)
     
     # match dataframes by name
-    employee_dataframe = employee.fuzzy_merge_df1_df2_df3(df1_and_df2, df3, 'Name', 'Name', threshold=50)
+    employee_dataframe = employee.fuzzy_merge_df1_df2_df3(df1_and_df2, df3, 'name', 'name', threshold=75)
+#    employee_dataframe = employee.remove_column(employee_dataframe, 'name', 'matches')
     
     # write pandas dataframe to exel file.
     employee.write_dataframe_to_exel(employee_dataframe)
-    
-    
-    
-    
-    
-    
-    
